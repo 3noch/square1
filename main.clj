1289dc39feb253f8d1d5a27303e04089a3216c2d
exupero <eric.shull@gmail.com>
2011-09-18 14:55:17 -0400

    Macro fun.

--- main.clj
+++ main.clj
@@ -31,19 +31,26 @@
 
 (defn transformation-hash-map-from-seq [seq]
   (apply hash-map-pairs
-    (for [i (-> seq count dec range) :let [j (inc i)]] 
-      [(nth seq i) (nth seq j)])))
+    (for [i (-> seq count dec range)]
+      [(nth seq i) (nth seq (inc i))])))
 
 (defn transformation [key & seqs]
   (let [t (apply merge
-            (for [seq seqs] (transformation-hash-map-from-seq seq)))]
+            (for [seq seqs]
+              (transformation-hash-map-from-seq seq)))]
     [key (transformation-func t)]))
 
-(def transformations
-  (hash-map-pairs
-    (transformation :T [3 6 5 4 3] [20 30 60 50 20])
-    (transformation :B [1 8 7 2 1] [10 40 70 80 10])
-    (transformation :t [1 2 4 3 1] [10 20 30 40 10])
-    (transformation :b [5 6 8 7 5] [50 60 70 80 50])))
+(defmacro def-transformations [& ts]
+  `(def transformations
+    (apply hash-map-pairs
+      ~@(for [tdata# ts]
+        (apply transformation
+          (concat [(keyword (first tdata#))] (rest tdata#)))))))
+
+(def-transformations
+  (T [3 6 5 4 3] [20 30 60 50 20])
+  (B [1 8 7 2 1] [10 40 70 80 10])
+  (t [1 2 4 3 1] [10 20 30 40 10])
+  (b [5 6 8 7 5] [50 60 70 80 50]))
 
 (println ((:t transformations) base-arrangement))
