e4df85607d87b1c8e2d880b57eb2dbc163f62f90
exupero <eric.shull@gmail.com>
2011-09-18 12:55:31 -0400

    Refactoring to make defining transformations more pleasant.

--- main.clj
+++ main.clj
@@ -1,23 +1,49 @@
 (require '[clojure.string :as str])
 
-(defn transformation [t]
-  (fn [arrangement]
-    (apply hash-map
-      (flatten
-        (for [piece (keys arrangement) :let [position (get arrangement piece)]]
-          [piece (get t position position)])))))
+; Utility functions
 
-(def base-arrangement {1 1, 2 2, 3 3, 4 4, 5 5, 6 6, 7 7, 8 8, 10 10, 20 20, 30 30, 40 40, 50 50, 60 60, 70 70, 80 80})
+(defn hash-map-pairs [& pairs]
+  (apply hash-map (flatten pairs)))
+
+(defn twin-pair [& items]
+  (for [item items] [item item]))
+
+; Data
+
+(def needed
+  (hash-map-pairs
+    (map #(str/split % #":") *command-line-args*)))
+
+(def base-arrangement
+  (apply hash-map-pairs
+    (apply twin-pair
+      (concat
+        (range 1 9)
+        (range 10 90 10)))))
+
+; Core logic
+
+(defn transformation-func [t]
+  (fn [arrangement]
+    (hash-map-pairs
+      (for [piece (keys arrangement) :let [position (get arrangement piece)]]
+        [piece (get t position position)]))))
+
+(defn transformation-hash-map-from-seq [seq]
+  (apply hash-map-pairs
+    (for [i (-> seq count dec range) :let [j (inc i)]] 
+      [(nth seq i) (nth seq j)])))
+
+(defn transformation [key & seqs]
+  (let [t (apply merge
+            (for [seq seqs] (transformation-hash-map-from-seq seq)))]
+    [key (transformation-func t)]))
 
 (def transformations
-  {:T (transformation {3 6, 6 5, 5 4, 4 3, 20 30, 30 60, 60 50, 50 20})
-   :B (transformation {1 8, 8 7, 7 2, 2 1, 10 40, 40 70, 70 80, 80 10})
-   :t (transformation {1 2, 2 4, 4 3, 3 1, 10 20, 20 30, 30 40, 40 10})
-   :b (transformation {5 6, 6 8, 8 7, 7 5, 50 60, 60 70, 70 80, 80 50})})
-
-(def needed 
-  (apply hash-map 
-    (flatten 
-      (map #(str/split % #":") *command-line-args*))))
+  (hash-map-pairs
+    (transformation :T [3 6 5 4 3] [20 30 60 50 20])
+    (transformation :B [1 8 7 2 1] [10 40 70 80 10])
+    (transformation :t [1 2 4 3 1] [10 20 30 40 10])
+    (transformation :b [5 6 8 7 5] [50 60 70 80 50])))
 
 (println ((:t transformations) base-arrangement))
