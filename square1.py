e98d6edf92c36b54293d038ea4653df13f5f4803
exupero <eric.shull@gmail.com>
2011-09-22 16:20:58 -0400

    Improved preformance of Python version, though it isn't actually using streams of any kind... (Turned out to be slow. That'll teach me to name branches based on implementation.)

--- square1.py
+++ square1.py
@@ -2,149 +2,188 @@ from itertools import count, product
 import sys
 
 
-def T(piece):
-    return {
-        1: 1,
-        2: 2,
-        3: 6,
-        4: 3,
-        5: 4,
-        6: 5,
-        7: 7,
-        8: 8,
-        10: 10,
-        20: 30,
-        30: 60,
-        40: 40,
-        50: 20,
-        60: 50,
-        70: 70,
-        80: 80,
-    }[piece]
+def transformation(name, d):
+    def _transformation(piece):
+        return d[piece]
 
-def B(piece):
-    return {
-        1: 8,
-        2: 1,
-        3: 3,
-        4: 4,
-        5: 5,
-        6: 6,
-        7: 2,
-        8: 7,
-        10: 40,
-        20: 20,
-        30: 30,
-        40: 70,
-        50: 50,
-        60: 60,
-        70: 80,
-        80: 10,
-    }[piece]
+    _transformation.__name__ = name
+    return _transformation
 
-def t(piece):
-    return {
-        1: 2,
-        2: 4,
-        3: 1,
-        4: 3,
-        5: 5,
-        6: 6,
-        7: 7,
-        8: 8,
-        10: 20,
-        20: 30,
-        30: 40,
-        40: 10,
-        50: 50,
-        60: 60,
-        70: 70,
-        80: 80,
-    }[piece]
 
-def b(piece):
-    return {
-        1: 1,
-        2: 2,
-        3: 3,
-        4: 4,
-        5: 6,
-        6: 8,
-        7: 5,
-        8: 7,
-        10: 10,
-        20: 20,
-        30: 30,
-        40: 40,
-        50: 60,
-        60: 70,
-        70: 80,
-        80: 50,
-    }[piece]
+T = transformation('T', {
+    1: 1,
+    2: 2,
+    3: 6,
+    4: 3,
+    5: 4,
+    6: 5,
+    7: 7,
+    8: 8,
+    10: 10,
+    20: 30,
+    30: 60,
+    40: 40,
+    50: 20,
+    60: 50,
+    70: 70,
+    80: 80,
+})
 
-def a(piece):
-    return {
-        1: 4,
-        2: 1,
-        3: 3,
-        4: 2,
-        5: 7,
-        6: 5,
-        7: 6,
-        8: 8,
-        10: 40,
-        20: 20,
-        30: 10,
-        40: 30,
-        50: 80,
-        60: 50,
-        70: 70,
-        80: 60,
-    }[piece]
+B = transformation('B', {
+    1: 8,
+    2: 1,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 2,
+    8: 7,
+    10: 40,
+    20: 20,
+    30: 30,
+    40: 70,
+    50: 50,
+    60: 60,
+    70: 80,
+    80: 10,
+})
 
-def c(piece):
-    return {
-        1: 4,
-        2: 3,
-        3: 1,
-        4: 2,
-        5: 7,
-        6: 6,
-        7: 5,
-        8: 8,
-        10: 40,
-        20: 20,
-        30: 10,
-        40: 30,
-        50: 80,
-        60: 50,
-        70: 70,
-        80: 60,
-    }[piece]
+t = transformation('t', {
+    1: 2,
+    2: 4,
+    3: 1,
+    4: 3,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    10: 20,
+    20: 30,
+    30: 40,
+    40: 10,
+    50: 50,
+    60: 60,
+    70: 70,
+    80: 80,
+})
 
-def s(piece):
-    return {
-        1: 7,
-        2: 5,
-        3: 1,
-        4: 8,
-        5: 6,
-        6: 2,
-        7: 3,
-        8: 4,
-        10: 40,
-        20: 20,
-        30: 20,
-        40: 10,
-        50: 60,
-        60: 50,
-        70: 80,
-        80: 70,
-    }[piece]
+b = transformation('b', {
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 6,
+    6: 8,
+    7: 5,
+    8: 7,
+    10: 10,
+    20: 20,
+    30: 30,
+    40: 40,
+    50: 60,
+    60: 70,
+    70: 80,
+    80: 50,
+})
+
+a = transformation('a', {
+    1: 4,
+    2: 1,
+    3: 3,
+    4: 2,
+    5: 7,
+    6: 5,
+    7: 6,
+    8: 8,
+    10: 40,
+    20: 20,
+    30: 10,
+    40: 30,
+    50: 80,
+    60: 50,
+    70: 70,
+    80: 60,
+})
+
+c = transformation('c', {
+    1: 4,
+    2: 3,
+    3: 1,
+    4: 2,
+    5: 7,
+    6: 6,
+    7: 5,
+    8: 8,
+    10: 40,
+    20: 20,
+    30: 10,
+    40: 30,
+    50: 80,
+    60: 50,
+    70: 70,
+    80: 60,
+})
+
+s = transformation('s', {
+    1: 7,
+    2: 5,
+    3: 1,
+    4: 8,
+    5: 6,
+    6: 2,
+    7: 3,
+    8: 4,
+    10: 40,
+    20: 30,
+    30: 20,
+    40: 10,
+    50: 60,
+    60: 50,
+    70: 80,
+    80: 70,
+})
+
+cbcbc = transformation('c b c b^-1 c', {
+    1: 3,
+    2: 4,
+    3: 2,
+    4: 1,
+    5: 8,
+    6: 6,
+    7: 7,
+    8: 5,
+    10: 40,
+    20: 30,
+    30: 10,
+    40: 20,
+    50: 70,
+    60: 60,
+    70: 50,
+    80: 80,
+})
+
+TB = transformation('T^2 B^2', {
+    1: 7,
+    2: 8,
+    3: 5,
+    4: 6,
+    5: 3,
+    6: 4,
+    7: 1,
+    8: 2,
+    10: 70,
+    20: 60,
+    30: 50,
+    40: 80,
+    50: 30,
+    60: 20,
+    70: 10,
+    80: 40,
+})
 
 
 def sequences():
-    transformations = [T, B, t, b, a, c, s]
+    transformations = [T, B, t, b, a, c, s, cbcbc, TB]
 
     for moves in count(1):
         print moves
@@ -156,10 +195,13 @@ needed = dict(map(int, item.split(':')) for item in sys.argv[1:])
 
 
 for sequence in sequences():
-    for piece, position in needed.iteritems():
-        result = reduce(lambda piece, t: t(piece), [piece] + list(sequence))
+    for piece in needed:
+        new_position = piece
 
-        if result != position:
+        for step in sequence:
+            new_position = step(new_position)
+
+        if new_position != needed[piece]:
             break
     else:
         print [func.__name__ for func in sequence]
